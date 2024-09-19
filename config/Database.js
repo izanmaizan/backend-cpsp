d3412e080803967fd4ae525eb2f766cc3295e4e9
izanmaizan <111477612+izanmaizan@users.noreply.github.com>
2024-09-19 08:35:03 +0000

    up

--- config/Database.js
+++ config/Database.js
@@ -6,7 +6,7 @@ dotenv.config();
 // ini untuk yang pakai server public
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1",
+  host: process.env.DB_HOST || "193.203.162.80",
   port: process.env.DB_PORT || 3306,  // Port default MySQL
   user: process.env.DB_USERNAME || "u232856820_cpsp",
   password: process.env.DB_PASSWORD || "your_password",  // Ganti dengan password yang sesuai

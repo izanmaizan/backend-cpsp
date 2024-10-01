603737ef3c1421da92330c6b926318ee7634ac1b
izanmaizan <111477612+izanmaizan@users.noreply.github.com>
2024-10-01 19:23:23 +0700

    update database

--- config/Database.js
+++ config/Database.js
@@ -4,14 +4,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'checkpoint-sig.site',
-  user: process.env.DB_USER || 'u232856820_cpsp',
-  password: process.env.DB_PASSWORD || '3&xN+g/f',
-  database: process.env.DB_NAME || 'u232856820_checkpoint',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'SIGcpsp2024',
+  database: process.env.DB_NAME || 'cpsp',
   port: process.env.DB_PORT || 3306,
 });
 
 export default pool;
+
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || 'checkpoint-sig.site',
+//   user: process.env.DB_USER || 'u232856820_cpsp',
+//   password: process.env.DB_PASSWORD || '3&xN+g/f',
+//   database: process.env.DB_NAME || 'u232856820_checkpoint',
+//   port: process.env.DB_PORT || 3306,
+// });
+
+// export default pool;
 
 
 // ini untuk yang pakai server local

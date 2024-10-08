8535a373e5c3267cc7a81dc51246b125522d6240
izanmaizan <111477612+izanmaizan@users.noreply.github.com>
2024-10-08 15:20:07 +0000

    fix

--- models/DetailModel.js
+++ models/DetailModel.js
@@ -2,11 +2,9 @@ import db from "../config/Database.js"; // Adjust import according to your setup
 
 export const getDetail = async (no_do) => {
   const [rows] = await db.execute(
-    "SELECT c.no_do, c.nama_petugas, l.lokasi AS titik_lokasi, c.tanggal, c.jam, c.dokumentasi, c.keterangan, c.no_hp, c.geofence_data, c.alamat, c.nama_pengemudi, c.no_truck, c.distributor, c.ekspeditur " +
-      "FROM check_points c " +
-      "LEFT JOIN lokasi l ON c.titik_lokasi = l.lokasi " +
-      "LEFT JOIN petugas p ON c.nama_petugas = p.nama_petugas " +
-      "WHERE c.no_do = ?",
+    "SELECT no_do, nama_petugas, titik_lokasi, tanggal, jam, dokumentasi, keterangan, no_hp, geofence_data, alamat, nama_pengemudi, no_truck, distributor, ekspeditur " +
+      "FROM check_points " +
+      "WHERE no_do = ?",
     [no_do]
   );
   return rows;

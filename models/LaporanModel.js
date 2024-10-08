249b288bccdc45580d16d8d297cb6adb7f249b4c
izanmaizan <111477612+izanmaizan@users.noreply.github.com>
2024-10-08 15:30:02 +0000

    update

--- models/LaporanModel.js
+++ models/LaporanModel.js
@@ -4,23 +4,20 @@ import db from "../config/Database.js";
 // Fungsi untuk mengambil data laporan dari check_points
 export const getLaporan = async () => {
   try {
-    const sql = `
-      SELECT 
-          no_do,
-          nama_petugas,
-          titik_lokasi,
-          tanggal,
-          jam,
-          dokumentasi,
-          keterangan,
-          no_hp,
-          geofence_data,
-          alamat,
-          nama_pengemudi,
-          no_truck,
-          distributor,
-          ekspeditur
-      FROM check_points
+    const sql = `SELECT 
+    p.nama_petugas AS petugas,
+    l.lokasi AS lokasi,
+    cp.no_do,
+    cp.no_truck,
+    cp.nama_pengemudi,
+    cp.distributor,
+    cp.ekspeditur,
+    cp.tanggal,
+    cp.jam
+FROM check_points cp
+LEFT JOIN lokasi l ON cp.titik_lokasi = l.id_lokasi
+LEFT JOIN petugas p ON cp.nama_petugas = p.id_petugas
+
     `;
 
     const [rows] = await db.execute(sql);

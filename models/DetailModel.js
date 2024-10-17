1bc07f82512e963d4f44708f363136dfa5056122
izanmaizan <111477612+izanmaizan@users.noreply.github.com>
2024-10-17 19:50:28 +0700

    menambahkan name dari akun yang melakukan check point

--- models/DetailModel.js
+++ models/DetailModel.js
@@ -2,7 +2,7 @@ import db from "../config/Database.js"; // Adjust import according to your setup
 
 export const getDetail = async (no_do) => {
   const [rows] = await db.execute(
-    "SELECT no_do, nama_petugas, titik_lokasi, tanggal, jam, dokumentasi, keterangan, no_hp, geofence_data, alamat, nama_pengemudi, no_truck, distributor, ekspeditur " +
+    "SELECT no_do, nama_petugas, titik_lokasi, tanggal, jam, dokumentasi, keterangan, no_hp, geofence_data, alamat, nama_pengemudi, no_truck, distributor, ekspeditur, name" +
       "FROM check_points " +
       "WHERE no_do = ?",
     [no_do]
